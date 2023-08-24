9f964b59ccd9748ce6686e94cc91af004c2ffeaf
alimukhtar53 <alimukhtar52@gmail.com>
2023-08-24 07:25:56 +0500

    fixed theme toggle button issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,15 +14,13 @@ const Navbar = async (props: Props) => {
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
         <Link href={"/"}>
-          <Logo></Logo>
+          <Logo />
         </Link>
 
         <div className="flex items-center focus:ring-0 focus:ring-offset-0 border-none">
+          <ModeToggle className="mr-3" />
           {session?.user ? (
-            <div className="flex gap-4 justify-center items-center">
-              <ModeToggle />
-              <UserAccountNav user={session.user} />
-            </div>
+            <UserAccountNav user={session.user} />
           ) : (
             <SignInButton text="Sign In" />
           )}
